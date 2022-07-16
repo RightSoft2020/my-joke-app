8de947018e256185dece6c3c7673f78e6ba309fc
RightSoft2020 <right.soft@yahoo.com>
2022-07-17 01:21:10 +0400

    create and set up project. closes #1

---
+++ src/main/java/com/dhouse/jokeapp/myjokeapp/MyJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.dhouse.jokeapp.myjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJokeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJokeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dhouse/jokeapp/myjokeapp/MyJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhouse.jokeapp.myjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJokeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
