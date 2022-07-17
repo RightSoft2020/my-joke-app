b370e1e2fc7a34c9bad1ae45f768d96e26cffab5
RightSoft2020 <right.soft@yahoo.com>
2022-07-18 00:16:27 +0400

    Map to context root ('/'). closes #5

--- src/main/java/com/dhouse/jokeapp/myjokeapp/controller/JokeController.java
+++ src/main/java/com/dhouse/jokeapp/myjokeapp/controller/JokeController.java
@@ -1,7 +1,24 @@
 package com.dhouse.jokeapp.myjokeapp.controller;
 
+import com.dhouse.jokeapp.myjokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JokeController {
+
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping("/")
+    public String showJoke(){
+        return "showjoke";
+    }
 }
+
--- src/main/java/com/dhouse/jokeapp/myjokeapp/services/JokeServiceImp.java
+++ src/main/java/com/dhouse/jokeapp/myjokeapp/services/JokeServiceImp.java
@@ -1,16 +1,20 @@
 package com.dhouse.jokeapp.myjokeapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+@Primary
 @Service
-public class JokeServiceImp {
+public class JokeServiceImp implements JokeService {
     private final ChuckNorrisQuotes norrisQuotes;
 
     public JokeServiceImp(){
         norrisQuotes = new ChuckNorrisQuotes();
     }
-    String getJoke(){
+
+    @Override
+    public String getJoke() {
         return norrisQuotes.getRandomQuote();
     }
 }
